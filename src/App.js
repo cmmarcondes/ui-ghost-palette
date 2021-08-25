d8ed3922a68217383f1571e8e195fbd4fda50ca5
carlos <carlos.santos@fcamara.com.br>
2021-08-25 14:41:40 -0300

    feat: adiciona o input e obtem a musica atraves do arquivo selecionado

--- src/App.js
+++ src/App.js
@@ -4,34 +4,20 @@ import P5Wrapper from 'react-p5-wrapper';
 import React from 'react';
 import './App.css';
 import './util/addons/p5.sound';
-import mymusic from "./assets/teste.mp3";
 
 function App() {
   var song;
   let fft;
   let button;
 
-  function toggleSong() {
-    if (song.isPlaying()) {
-      song.pause();
-    } else {
-      song.play();
-    }
-  }
-
   const sketch = (p5) => {
-    p5.preload = () => {
-      p5.soundFormats('mp3', 'ogg');
-      song = new window.p5.prototype.loadSound(mymusic);
-    }
-
     p5.setup = () => {
+      p5.createFileInput((event) => song = new window.p5.prototype.loadSound(event.file));
       p5.createCanvas(256, 256);
       p5.colorMode(p5.HSB);
       p5.angleMode(p5.DEGREES);
       button = p5.createButton('toggle');
       button.mousePressed(toggleSong);
-      song.play();
       fft = new window.p5.FFT(0.9, 128);
     };
 
@@ -50,6 +36,15 @@ function App() {
         p5.stroke(i, 255, 255);
         p5.line(0, 0, x, y);
       }
+    }
+  }
+
+  function toggleSong() {
+    if (song.isPlaying()) {
+      song.pause();
+    } else {
+      song.play();
+      console.log(fft)
     }
   }
 
