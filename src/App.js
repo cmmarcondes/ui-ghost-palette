eea4e6dfe036cb69da1db7ab3122f47dcf1d1f3a
carlos <carlos.santos@fcamara.com.br>
2021-08-28 11:26:34 -0300

    feat: altera o contexto do projeto

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-/* eslint-disable no-undef */
-import P5Wrapper from 'react-p5-wrapper';
+
 import React from 'react';
-import './util/addons/p5.sound';
-import { Container } from './styles';
-import Button from './elements/Button';
-import FileInput from './elements/FileInput';
-import { useApplicationContext } from './context/ApplicationContext';
+import Routes from './routes';
 
 function App() {
-  const { song } = useApplicationContext();
-
-  const sketch = (p5) => {
-    p5.setup = () => {
-      new window.p5.FFT(0.9, 128);
-    };
-  }
-
-  function toggleSong() {
-    if (song.isPlaying()) {
-      song.pause();
-    } else {
-      song.play();
-    }
-  }
-
-  return (
-    <Container>
-      <P5Wrapper sketch={sketch} />
-      <FileInput />
-      <Button toggleSong={toggleSong}>gerar paleta</Button>
-    </Container>
-  
-  );
+  return(
+    <Routes />
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ApplicationContextProvider from './context/ApplicationContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApplicationContextProvider>
       <App />
-    </ApplicationContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
