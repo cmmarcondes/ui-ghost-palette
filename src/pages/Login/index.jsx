27f8c4a287baa4880e6b50cd93e5aae0ec31b9f0
dinocarlos <carlos.santos168@fatec.sp.gov.br>
2021-11-08 21:52:46 -0300

    feat: redireciona p/ home apos login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,34 @@
-import React, { useCallback } from 'react'
-import ReactFacebookLogin from 'react-facebook-login'
-import { LoginService } from '../../service/LoginService';
-import { Container } from './styles'
+import React, { useCallback } from "react";
+import ReactFacebookLogin from "react-facebook-login";
+import { useHistory } from "react-router";
+import { LoginService } from "../../service/LoginService";
+import { Container } from "./styles";
 
 const Login = () => {
-    const facebookResponse = useCallback(async(res) => {
-        const loginService = new LoginService();
-        console.log(res);
-        //await loginService.logar(res.name, res.email);
+  const history = useHistory();
 
-    }, []);
+  const facebookResponse = useCallback(async (res) => {
+    const loginService = new LoginService();
+    await loginService
+      .logar({
+        name: res.name,
+        email: res.email,
+        userID: res.userID,
+      })
+      .then(() => {
+        history.push("/home");
+      });
+  }, []);
 
-    return (
-        <Container>
-            <ReactFacebookLogin
-                appId="3006086256296516"
-                fields="name, email, picture"
-                callback={facebookResponse}
-            />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <ReactFacebookLogin
+        appId="3006086256296516"
+        fields="name, email, picture"
+        callback={facebookResponse}
+      />
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
